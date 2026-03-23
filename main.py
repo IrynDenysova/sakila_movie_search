4f1e9c7f53c77d160d3999c83c97b19d830f0eda
IrynDenysova <iriska.ermak@gmail.com>
2026-03-23 19:20:56 +0100

    added search full genres/years

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import pymysql
 from datetime import datetime
 
 
-def search_film(connection):
+def search_by_title(connection):
     title = input("Enter film title or part of title: ").strip().lower()
     with connection.cursor() as cursor:
         cursor.execute("""
@@ -38,6 +38,25 @@ def view_genre_years(connection):
             print(f"-{genre} - ({min_years} -{max_year})")
 
 
+def search_by_genre_years(connection):
+    with connection.cursor() as cursor:
+        search_genre = input("Enter the genre, you are looking for: ").strip().lower()
+        start_year = input("Enter the year you are searching from: ")
+        end_year = input("Enter the year you are searching to (Enter if the year is the same): ") or start_year
+        cursor.execute("""
+            SELECT f.title, f.release_year
+            FROM film as f
+            JOIN film_category as fc
+            ON f.film_id = fc.film_id
+            JOIN category as c
+            ON c.category_id = fc.category_id
+            WHERE c.name = %s AND f.release_year BETWEEN %s AND %s""",
+                                    (search_genre,start_year,end_year,))
+        all_found_movies = cursor.fetchall()
+        for film,year in all_found_movies:
+            print(f"-{film},{year}")
+
+
 
 
 
@@ -54,10 +73,11 @@ with pymysql.connect(**mysql_connector.config) as connection:
 
             choice = input("Enter your choice: ")
             if choice == "1":
-                search_film(connection)
+                search_by_title(connection)
 
             elif choice == "2":
                 view_genre_years(connection)
+                search_by_genre_years(connection)
 
 
             elif choice == "3":
